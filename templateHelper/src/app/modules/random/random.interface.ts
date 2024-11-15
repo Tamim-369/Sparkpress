fddd96b22b271174a4926477b2098153ae8c305f
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-11-15 10:32:29 +0600

    adding more functionalties to helper cli

--- templateHelper/src/app/modules/random/random.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Model, Types } from 'mongoose';
-
-export type IRandom = {
-  field1: string;
-  field2: Types.ObjectId;
-  field3: [Types.ObjectId]
-};
-
-export type RandomModel = Model<IRandom>;
